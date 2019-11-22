763ff64b7f2e1dc9ba3fd17305ae285e4bc5de15
Alex Cassol <cassol@automatizesistemas.com.br>
2019-11-22 17:26:34 -0300

    code review

--- src/Printer/PrinterEscPos.fs
+++ src/Printer/PrinterEscPos.fs
@@ -32,120 +32,127 @@ type PrinterEscPos(docDef:DocumentDefinition) =
         let times = getNTimes None condensed           
         (fst times, sep + dashLine (snd times) + sep )
                 
-    member this.Commands = (this :> ICommandEscPos)
-    
+    member this.Commands = (this :> ICommandEscPos)    
    
     interface ICommandEscPos with
         
         member this.Bold(arg1: OnOff): byte array = 
             match arg1 with
-            | On -> ToByteArray [|27; 69; 1|] 
-            | Off -> ToByteArray [|27; 69; 0|] 
+            | On -> [|27; 69; 1|] 
+            | Off -> [|27; 69; 0|]
+            |> ToByteArray
 
         member this.BoldText(arg1: string): byte array = 
-            Array.concat [  
-                (this :> ICommandEscPos).Bold(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).Bold(Off) ;
-                ToByte("\n")]
+            [ this.Commands.Bold(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.Bold(Off) ;
+              ToByte("\n")]
+            |> Array.concat
 
         member this.Expanded(arg1: OnOff): byte array = 
             match arg1 with
-            | On -> ToByteArray [|29; 33; 16|] 
-            | Off -> ToByteArray [|29; 33; 0|] 
+            | On -> [|29; 33; 16|] 
+            | Off -> [|29; 33; 0|]
+            |> ToByteArray
 
         member this.ExpandedText(arg1: string): byte array = 
-            Array.concat [  
-                (this :> ICommandEscPos).Expanded(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).Expanded(Off) ;
-                ToByte("\n")]
+            [ this.Commands.Expanded(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.Expanded(Off) ;
+              ToByte("\n")]
+            |> Array.concat 
 
         member this.Condensed(arg1: OnOff): byte array = 
             match arg1 with
-            | On -> ToByteArray [|27; 33; 1|] 
-            | Off -> ToByteArray [|27; 33; 0|] 
+            | On -> [|27; 33; 1|] 
+            | Off -> [|27; 33; 0|]
+            |> ToByteArray
 
         member this.CondensedText(arg1: string): byte array = 
-            Array.concat [  
-                (this :> ICommandEscPos).Condensed(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).Condensed(Off) ;
-                ToByte("\n")]
+            [ this.Commands.Condensed(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.Condensed(Off) ;
+              ToByte("\n")]
+            |> Array.concat
 
         member this.Underline(arg1: OnOff): byte array = 
             match arg1 with
-            | On -> ToByteArray [|27; 45; 1|] 
-            | Off -> ToByteArray [|27; 45; 0|] 
+            | On -> [|27; 45; 1|] 
+            | Off -> [|27; 45; 0|]
+            |> ToByteArray
 
         member this.UnderlineText(arg1: string): byte array = 
-            Array.concat [  
-                (this :> ICommandEscPos).Underline(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).Underline(Off);
-                ToByte("\n")]
+            [ this.Commands.Underline(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.Underline(Off);
+              ToByte("\n")]
+            |> Array.concat
 
         member this.OpenCashDrawer: byte array = 
             ToByteArray [|27; 112; 0; 60; 120|]  
 
         member this.Italic(arg1:OnOff): byte array = 
             match arg1 with
-            | On -> ToByteArray [|27; 52|] 
-            | Off -> ToByteArray [|27; 53|] 
+            | On -> [|27; 52|] 
+            | Off -> [|27; 53|]
+            |> ToByteArray
 
         member this.ItalicText(arg1:string): byte array =
-            Array.concat [  
-                (this :> ICommandEscPos).Italic(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).Italic(Off);
-                ToByte("\n")]
+            [ this.Commands.Italic(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.Italic(Off);
+              ToByte("\n")]
+            |> Array.concat
 
         member this.DoubleStrike(arg1:OnOff): byte array = 
             match arg1 with
-            | On -> ToByteArray [|27; 71; 1|] 
-            | Off -> ToByteArray [|27; 71; 0|] 
+            | On -> [|27; 71; 1|] 
+            | Off -> [|27; 71; 0|]
+            |> ToByteArray
 
         member this.DoubleStrikeText(arg1:string): byte array =
-            Array.concat [  
-                (this :> ICommandEscPos).DoubleStrike(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).DoubleStrike(Off);
-                ToByte("\n")]
+            [ this.Commands.DoubleStrike(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.DoubleStrike(Off);
+              ToByte("\n")]
+            |> Array.concat
 
         member this.Invert(arg1: OnOff): byte array = 
             match arg1 with
-            | On -> ToByteArray [|29; 66; 1|] 
-            | Off -> ToByteArray [|29; 66; 0|] 
+            | On -> [|29; 66; 1|] 
+            | Off -> [|29; 66; 0|]
+            |> ToByteArray
 
         member this.InvertText(arg1: string): byte array = 
-            Array.concat [  
-                (this :> ICommandEscPos).Invert(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).Invert(Off);
-                ToByte("\n")]
-        
+            [ this.Commands.Invert(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.Invert(Off);
+              ToByte("\n")]
+            |> Array.concat
+            
         member this.FontA(_:OnOff): byte array = 
             ToByteArray [|27; 77; 48|] 
             
         member this.FontAText(arg1:string): byte array =
-            Array.concat [  
-                (this :> ICommandEscPos).FontA(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).FontA(Off);
-                ToByte("\n")]
+            [ this.Commands.FontA(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.FontA(Off);
+              ToByte("\n")]
+            |> Array.concat
 
         member this.FontB(arg1:OnOff): byte array = 
             match arg1 with
-            | On -> ToByteArray [|27; 77; 49|] 
-            | Off -> ToByteArray [|27; 77; 48|] 
+            | On -> [|27; 77; 49|] 
+            | Off -> [|27; 77; 48|]
+            |> ToByteArray
             
         member this.FontBText(arg1:string): byte array =
-            Array.concat [  
-                (this :> ICommandEscPos).FontB(On) ; 
-                ToByte(arg1) ; 
-                (this :> ICommandEscPos).FontB(Off);
-                ToByte("\n")]
-
+            [ this.Commands.FontB(On) ; 
+              ToByte(arg1) ; 
+              this.Commands.FontB(Off);
+              ToByte("\n")]
+            |> Array.concat
+            
         member this.Left: byte array = 
             ToByteArray [|27; 97; 0|] 
 
@@ -157,8 +164,9 @@ type PrinterEscPos(docDef:DocumentDefinition) =
 
         member this.PaperCut(arg1:CutType) : byte array =
             match arg1 with
-            | Partial -> ToByteArray [|29; 86; 65; 3|] 
-            | Full -> ToByteArray [|29; 86; 65; 3|] 
+            | Partial -> [|29; 86; 65; 3|] 
+            | Full -> [|29; 86; 65; 3|]
+            |> ToByteArray
 
         member this.AutoTest: byte array =
             ToByteArray [|29; 40; 65; 2; 0; 0; 2|] 
@@ -168,22 +176,22 @@ type PrinterEscPos(docDef:DocumentDefinition) =
         
         member this.Separator(?cols:int): byte array =
             let nTimes = match getNTimes cols (Some false) with | _, n -> n                
-            (this :> ICommandEscPos).FontAText(dashLine nTimes)
+            this.Commands.FontAText(dashLine nTimes)
 
         member this.SeparatorCondensed(?cols:int): byte array =
             let nTimes = match getNTimes cols (Some true) with | _, n -> n                           
-            (this :> ICommandEscPos).CondensedText(dashLine nTimes)  
+            this.Commands.CondensedText(dashLine nTimes)  
         
         member this.OpenCloseTable(?condensed:bool): byte array =
             match prepareTable "+" condensed with
             | true, s ->  
-                (this :> ICommandEscPos).CondensedText(s)
+                this.Commands.CondensedText(s)
             | _, s ->
-                (this :> ICommandEscPos).FontAText(s)                 
+                this.Commands.FontAText(s)                 
             
         member this.LineTable(?condensed:bool): byte array =
             match prepareTable "|" condensed with
             | true, s ->  
-                (this :> ICommandEscPos).CondensedText(s)
+                this.Commands.CondensedText(s)
             | _, s ->
-                (this :> ICommandEscPos).FontAText(s)
+                this.Commands.FontAText(s)
